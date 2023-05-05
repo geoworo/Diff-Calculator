6ef45adb772d6e39b2f4f33bbcfdcc72bc6b4edb
George Voronin <bordernormal@gmail.com>
2023-05-06 01:17:56 +0700

    Add picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,31 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
+
 public class App {
+
+    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    private boolean usageHelpRequested = false;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionInfoRequested;
+
     public static void main(String[] args) {
+        App app = CommandLine.populateCommand(new App(), args);
+        CommandLine commandLine = new CommandLine(new App());
+        commandLine.parseArgs(args);
+        if (app.usageHelpRequested) {
+            CommandLine.usage(new App(), System.out);
+            return;
+        } else if (commandLine.isVersionHelpRequested()) {
+            commandLine.printVersionHelp(System.out);
+            return;
+        }
         System.out.println("Hello World!");
     }
 }
