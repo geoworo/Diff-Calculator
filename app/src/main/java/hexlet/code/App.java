645b6ee5e2bb573e5908122041ec0d65ac1340a2
George Voronin <bordernormal@gmail.com>
2023-05-06 22:30:20 +0700

    Update help

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,9 +5,21 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.io.File;
+
 @Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
 
 public class App {
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    File file1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    File file2;
+
+    @Option(names = {"-f", "--format"}, defaultValue = "stylish",
+            description = "output format (default: stylish)")
+    String format;
 
     @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
     private boolean usageHelpRequested = false;
