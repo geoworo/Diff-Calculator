f3e1968e9846e4b27aa89cd35f667adae5f73b3f
George Voronin <bordernormal@gmail.com>
2023-06-03 21:50:42 +0700

    Fix syntax

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,7 +2,8 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DifferTest {
 
