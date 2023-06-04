e812309930af30bcd8343e26596409bffeabc06b
George Voronin <bordernormal@gmail.com>
2023-06-04 23:34:23 +0700

    Fix linter bs

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -2,27 +2,46 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class DifferTest{
+public class DifferTest {
 
     @Test
     public void testGen() throws Exception {
-        Exception exception = assertThrows(Exception.class, () -> Differ.generate("fileone.json", "filetwo.json"));
+        String fileone = "src/test/resources/ffile2.json";
+        String filetwo = "src/test/resources/sfile2.json";
+
+        Exception exception = assertThrows(Exception.class, () -> Differ.generate("nonexistent.json", "nonexistent.json"));
         String expectedMessage = "does not exist";
         String actualMessage = exception.getMessage();
 
-        Exception exception2 = assertThrows(Exception.class, () -> Differ.generate("src/test/resources/ffile2.json", "src/test/resources/sfile2.json"));
+        Exception exception2 = assertThrows(Exception.class, () -> Differ.generate(fileone, filetwo));
         String expectedMessage2 = "does not exist";
         String actualMessage2 = exception2.getMessage();
 
         assertTrue(actualMessage.contains(expectedMessage));
         assertTrue(actualMessage2.contains(expectedMessage2));
 
-        String expected1 = "{\n   count: 3\n - country: Colombia\n + date: 1720\n - items: [rum, pirates, ships]\n + items: [ships]\n}";
+        String expected1 = """
+         {
+            count: 3
+          - country: Colombia
+          + date: 1720
+          - items: [rum, pirates, ships]
+          + items: [ships]
+         }""";
         assertEquals(expected1, Differ.generate("src/test/resources/ffile.json", "src/test/resources/sfile.json"));
 
-        String expected2 = "{\n   age: 23\n   firstName: Kyle\n   id: 1234567\n   middleName: Connor\n   secondName: Black\n}";
+        String expected2 = """
+        {
+           age: 23
+           firstName: Kyle
+           id: 1234567
+           middleName: Connor
+           secondName: Black
+        }""";
         assertEquals(expected2, Differ.generate("src/test/resources/ffile1.json", "src/test/resources/sfile1.json"));
 
     }
