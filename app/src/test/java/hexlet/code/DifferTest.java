a9039ae8fcb1b5a3b8e6f05ee8ff6e3f5e5479d2
George Voronin <bordernormal@gmail.com>
2023-06-18 01:56:38 +0700

    Separate lines

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -66,11 +66,16 @@ public class DifferTest {
         assertEquals(expected3, diff4);
 
         StringBuilder sb2 = new StringBuilder();
-        sb2.append("{\n  \"oceans\" : {\n    \"FirstFile\" : [ \"Pacific\", \"Atlantic\", \"Indian\", \"Southern\", \"Arctic\" ],\n");
-        sb2.append("    \"SecondFile\" : [ \"North Atlantic\", \"Indian\", \"Pacific\" ]\n  },\n  \"period\" : {\n    \"FirstFile\" : {\n");
-        sb2.append("      \"beginning\" : 1500,\n      \"end\" : 1830\n    },\n    \"SecondFile\" : {\n      \"beginning\" : 1650,\n");
-        sb2.append("      \"end\" : 1730\n    }\n  },\n  \"pirates\" : {\n    \"SecondFile\" : \"Henry Every\"\n  },\n  \"region\" : {\n");
-        sb2.append("    \"FirstFile\" : \"the Caribbean\"\n  },\n  \"topic\" : {\n    \"FirstFile\" : \"piracy\",\n");
+        sb2.append("{\n  \"oceans\" : {\n    \"FirstFile\" : [ \"Pacific\", ");
+        sb2.append("\"Atlantic\", \"Indian\", \"Southern\", \"Arctic\" ],\n");
+        sb2.append("    \"SecondFile\" : [ \"North Atlantic\", \"Indian\", \"Pacific\"");
+        sb2.append(" ]\n  },\n  \"period\" : {\n    \"FirstFile\" : {\n");
+        sb2.append("      \"beginning\" : 1500,\n      \"end\" : 1830\n    },\n    ");
+        sb2.append("\"SecondFile\" : {\n      \"beginning\" : 1650,\n");
+        sb2.append("      \"end\" : 1730\n    }\n  },\n  \"pirates\" : {\n    ");
+        sb2.append("\"SecondFile\" : \"Henry Every\"\n  },\n  \"region\" : {\n");
+        sb2.append("    \"FirstFile\" : \"the Caribbean\"\n  },\n  ");
+        sb2.append("\"topic\" : {\n    \"FirstFile\" : \"piracy\",\n");
         sb2.append("    \"SecondFile\" : \"piracy\"\n  }\n}\n");
 
         String expected = sb2.toString();
